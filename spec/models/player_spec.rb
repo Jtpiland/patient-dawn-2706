124180b2954f0b8ce59d8561bf133d7285063b25
Jacob Piland <piland.jacob@gmail.com>
2021-08-04 11:32:39 -0500

    add average age test to player model spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Player do
+  before :each do
+    @team_1 = Team.create!(hometown: 'Lawrence', nickname: 'Jayhawks')
+    
+    @team_1.players.create!(name: 'Billy Bigshoes', age: 18)
+    @team_1.players.create!(name: 'Julius Wright', age: 22)
+    @team_1.players.create!(name: 'Russel Robinson', age: 20)
+  end
   describe 'relationships' do
     it { should belong_to(:team) }
+  end
+
+  describe '::class methods' do
+    describe '::average_age' do
+      it 'can determine the average age of all players' do
+        expect(Player.average_age).to eq(20.0)
+      end
+    end
   end
 end
